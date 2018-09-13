fd94efcc1becd4833749f9d5588a693086753fac
sbasnet <basnetsanjaya7@gmail.com>
2018-09-13 14:46:48 -0400

    Factorial recursive

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -11,13 +11,26 @@ public class Factorial {
          * Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120.
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          */
-        int fact=1;
-        int n =5;
-        for (int i=1; i<=n; i++){
-            fact = fact *i;
+        //iteration method
+        int fact = 1;
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
         }
-    System.out.println("Factorial of " +n+ " is " + fact);
+        System.out.println("Factorial(iteration) of " + n + " is " + fact);
 
+        //recursion method
+        int factorial = fact(n);
+        System.out.println("Factorial(recursive) of "+n+ " is " + factorial);
+    }
+    static int fact(int n){
+        int result;
+        if (n==1){
+            return 1;
+        }else{
+            result=n*fact(n-1);
+            return result;
+        }
 
     }
 }
