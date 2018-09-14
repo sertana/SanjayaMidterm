6f32a44dc4f9a9dae3f79cccb5268e28452cc02b
sbasnet <basnetsanjaya7@gmail.com>
2018-09-14 14:50:14 -0400

    lowest diff added

--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -9,6 +9,15 @@ public class FindLowestDifference {
         int [] array1 = {30,12,5,9,2,20,33,1};
         int [] array2 = {18,25,41,47,17,36,14,19};
 
+        int minimum = array1[0];
+        for (int i=0; i<array1.length; i++) {
+            for (int j=0; j<array2.length; j++){
+                int diff = Math.abs(array1[i] -array2[j]);
+                if(diff<minimum)
+                    minimum=diff;
+            }
+        }
+    System.out.println(minimum);
 
 
 
